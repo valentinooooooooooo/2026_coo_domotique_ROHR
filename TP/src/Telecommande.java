dac9983a4d3fdadb793cbf37b17a1ad5a05fbb3c
Valentin ROHR <valentin.rohr9@etu.univ-lorraine.fr>
2026-04-03 09:20:50 +0200

    Initialisation du projet

---
+++ TP/src/Telecommande.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Telecommande {
+
+    private List<Lampe> lampes;
+
+    public Telecommande(){
+        throw new Error("code non écrit");
+    }
+    public void ajouterLampe(int IndiceLampe){
+        throw new Error("code non écrit");
+    }
+
+}
