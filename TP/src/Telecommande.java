24bc19ef5f835f8d66749134b10c8ffde177a7a7
Valentin ROHR <valentin.rohr9@etu.univ-lorraine.fr>
2026-04-03 09:35:25 +0200

    Squelette de la classe Telecommande

--- TP/src/Telecommande.java
+++ TP/src/Telecommande.java
@@ -7,8 +7,19 @@ public class Telecommande {
     public Telecommande(){
         throw new Error("code non écrit");
     }
-    public void ajouterLampe(int IndiceLampe){
+    public void ajouterLampe(Lampe lampe){
+        throw new Error("code non écrit");
+
+    }
+    public void desactiverLampe(int IndiceLampe){
         throw new Error("code non écrit");
     }
+    public void activerLampe(int IndiceLampe){
+        throw new Error("code non écrot");
+    }
+    public void ajouterTout(){
+        throw new Error("code non écrit");
+    }
+    
 
 }
